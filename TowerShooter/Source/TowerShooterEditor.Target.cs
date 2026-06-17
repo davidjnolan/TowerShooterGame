67e03082f72fe856a56d4abb922a18a3ec47ac33
DavidJNolan <davidjnolan101@gmail.com>
2026-06-17 21:00:51 +0100

    After Migrate to 5.7

--- TowerShooter/Source/TowerShooterEditor.Target.cs
+++ TowerShooter/Source/TowerShooterEditor.Target.cs
@@ -8,7 +8,7 @@ public class TowerShooterEditorTarget : TargetRules
 	public TowerShooterEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 
 		ExtraModuleNames.AddRange( new string[] { "TowerShooter" } );
 	}
